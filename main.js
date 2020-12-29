4eff17de1e7564479e341d3ed4a584a37965dcd4
sayanevidev <73514537+sayanevidev@users.noreply.github.com>
2020-12-29 17:18:29 +0530

    --- working for multiple inputs

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 import { crack, keyFinder, check } from './resolver.js'
-let x = "AIR ROZO";
-let arr = x.split(' ');
-let key = keyFinder(arr[0]);
-// console.log(key);
-let decryptedCode = crack(arr[1], key);
-// console.log(decryptedCode);
-let c = check(decryptedCode, arr[0]);
-if(c){
-    console.log("correct");
+let x = ["AIR ROZO", "LAND FAIJWJSOOFAMAU", "ICE STHSTSTVSASOS"];
+let y = ['SPACE'];
+for (let i = 0;i < x.length; i++){
+    let arr = x[i].split(' ');
+    let key = keyFinder(arr[0]);
+    let decryptedCode = crack(arr[1], key);
+    let c = check(decryptedCode, arr[0]);
+    if(c){
+        y.push(arr[0]);
+    }
 }
+// let arr = x.split(' ');
+// let key = keyFinder(arr[0]);
+// // console.log(key);
+// let decryptedCode = crack(arr[1], key);
+// // console.log(decryptedCode);
+// let c = check(decryptedCode, arr[0]);
+// if(c){
+//     console.log("correct");
+// }
+console.log(y);
 
